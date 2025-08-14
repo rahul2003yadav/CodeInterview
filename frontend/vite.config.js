9c7af8f15597959cc0c5b1a38abf4c9dadbb65b1
rahul2003yadav <rahul2003ryadav@gmail.com>
2025-08-14 12:33:58 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'public', // or any directory you want your build files in
+    emptyOutDir: true // cleans the directory before building
+  }
 })
+
