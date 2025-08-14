896a1323c4ac262259288c9c73467c940cf13099
rahul2003yadav <rahul2003ryadav@gmail.com>
2025-08-14 12:44:18 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'public', // or any directory you want your build files in
-    emptyOutDir: true // cleans the directory before building
-  }
+    outDir: 'dist',  // separate output directory from public
+    emptyOutDir: true
+  },
+  publicDir: 'public' // default static assets directory
 })
+
 
